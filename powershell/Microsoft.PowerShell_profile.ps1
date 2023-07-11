d88e3643ed92f2c82c29371fbfdcea1c525063bc
aryman <amandaryman@gmail.com>
2023-07-11 16:13:35 -0700

    functions live in the functions house

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,24 +3,6 @@ Set-Alias bso $ps_script_dir\BuildSOandNotify.ps1
 Set-Alias papercut "C:\Program Files (x86)\Changemaker Studios\Papercut SMTP\Papercut.Service.exe"
 Set-Alias showEnv "Get-ChildItem -Path Env:"
 
-
-function GetDotnetInfo {
-
-	Write-Host ">>NET Runtimes:"
-	dotnet --list-runtimes
-
-	Write-Host ">>list NET SDKs:"
-	dotnet --list-runtimes
-
-	Write-Host ">>check NET SDKs:"
-	dotnet sdk check
-
-	Write-Host ">>NET Framework:"
-	Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}' } | Select-Object PSChildName, version
-}
-
-Set-Alias which-dotnet GetDotnetInfo
-
 # OH MY POSHNESS
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/montys.omp.json' | Invoke-Expression
 
